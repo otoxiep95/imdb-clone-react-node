2f6d40a59470707a88f52836c46ddb6527a6fb09
Mindaugas Jukna <mind0004@stud.kea.dk>
2020-05-30 17:26:28 +0200

    Working on profile...

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Profile() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:9090/api/users", {
+            credentials: "include",
+        })
+            .then((data) => {
+                console.log(data);
+                setUsername(data.username);
+                setEmail(data.email);
+            });
+    }, []);
+
     return (
-        <h1>Profile</h1>
-    )
+        <div className="profile-container">
+            <h1>Name: {username}</h1>
+            <h1>Email: {email}</h1>
+        </div>
+    );
 }
