1347e42feed3b56e904ab2dca7ceac8ca53f2bf1
Mindaugas Jukna <mind0004@stud.kea.dk>
2020-05-30 11:29:17 +0200

    sign up

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,26 +1,49 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default function Login() {
+export default function Login({ setIsLogged }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const history = useHistory();
+
+    function login() {
+        fetch("http://127.0.0.1:9090/api/users/login", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        })
+            .then((response) => response.json())
+            .then((res) => {
+                console.log(res);
+                setIsLogged(true);
+                history.push("/");
+            });
+    }
 
     return (
         <div className="Login">
             <h1>Log in</h1>
             <form>
-                <input 
+                <input
                     type="text"
                     name="username"
                     placeholder="username"
                     onChange={e => setUsername(e.target.value)}
                 />
-                 <input 
+                <input
                     type="password"
                     name="password"
                     placeholder="password"
                     onChange={e => setPassword(e.target.value)}
                 />
-                <button>Log in</button>
+                <button type="button" onClick={login}>Login</button>
             </form>
             <div>
                 <p>Forgot your password? Reset it here</p>
--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,40 +1,62 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Signup() {
     const [username, setUsername] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const [confirmPassword, setConfirmPassword] = useState();
+    const history = useHistory();
+
+    function register() {
+        fetch("http://127.0.0.1:9090/api/users/register", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                email: email,
+                password: password,
+                confirm_password: confirmPassword,
+            }),
+        }).then((res) => {
+            console.log(res);
+            history.push("/login");
+        });
+    }
 
     return (
         <div className="Signup">
             <h1>Sign up</h1>
             <form>
-                <input 
+                <input
                     type="text"
                     name="username"
                     placeholder="username"
                     onChange={e => setUsername(e.target.value)}
                 />
-                <input 
+                <input
                     type="email"
                     name="email"
                     placeholder="email"
                     onChange={e => setEmail(e.target.value)}
                 />
-                <input 
+                <input
                     type="password"
                     name="password"
                     placeholder="password"
                     onChange={e => setPassword(e.target.value)}
                 />
-                <input 
+                <input
                     type="password"
                     name="confirmPassword"
                     placeholder="confirm password"
                     onChange={e => setConfirmPassword(e.target.value)}
                 />
-                <button>Sign up</button>
+                <button type="button" onClick={register}>Sign Up</button>
             </form>
         </div>
     )
