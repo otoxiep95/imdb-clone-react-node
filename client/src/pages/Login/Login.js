6b6e77914eb37fcc1e00a4ec9856a9ca5b80ab3e
Mindaugas Jukna <mind0004@stud.kea.dk>
2020-05-31 13:49:05 +0200

    ...

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import './Login.css';
 
 export default function Login(props) {
     const { setIsAuthenticated } = props;
@@ -40,27 +41,29 @@ export default function Login(props) {
 
     return (
         <div className="Login">
-            <h1>Log in</h1>
-            <form>
-                <input
-                    type="text"
-                    name="username"
-                    placeholder="username"
-                    onChange={e => setUsername(e.target.value)}
-                />
-                <input
-                    type="password"
-                    name="password"
-                    placeholder="password"
-                    onChange={e => setPassword(e.target.value)}
-                />
-                <button type="button" onClick={login}>Login</button>
-            </form>
-            <div>
-                <p>Forgot your password? Reset it here</p>
-            </div>
-            <div>
-                {error ? <p className="error">{error}</p> : ""}
+            <div className="loginSection">
+                <h1>Log in</h1>
+                <form className="LoginForm">
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="username"
+                        onChange={e => setUsername(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <button type="button" onClick={login}>Login</button>
+                </form>
+                <div>
+                    <p>Forgot your password? Reset it here</p>
+                </div>
+                <div>
+                    {error ? <p className="error">{error}</p> : ""}
+                </div>
             </div>
         </div>
     )
